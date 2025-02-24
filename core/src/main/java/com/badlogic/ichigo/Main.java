64655889cfda440e18bbde4a5f3a96e397ddb1a2
Matt K <matthew.b.kurtti@gmail.com>
2025-02-24 16:53:15 +0900

    init: initialize project with libgdx game library, draws bg image and character img

---
+++ core/src/main/java/com/badlogic/ichigo/Main.java
@@ -0,0 +1,82 @@
+package com.badlogic.ichigo;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main implements ApplicationListener {
+    Texture backgroundTexture;
+    Texture runnerTexture;
+
+
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+
+    @Override
+    public void create() {
+        // Prepare your application here.
+        backgroundTexture = new Texture("running-track.jpg");
+        runnerTexture = new Texture("runner.png");
+
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(16, 10);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // Resize your application here. The parameters represent the new window size.
+        viewport.update(width, height, true); // true centers the camera
+    }
+
+    @Override
+    public void render() {
+        // Draw your application here.
+        // organize code into three methods
+        input();
+        logic();
+        draw();
+    }
+
+    private void input() {
+
+    }
+
+    private void logic() {
+
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        // store the worldWidth and worldHeight as local variables for brevity
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight); // draw the background
+        spriteBatch.draw(runnerTexture, 0, 0, 1, 1); // draw the bucket
+
+        spriteBatch.end();
+    }
+
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void dispose() {
+        // Destroy application's resources here.
+    }
+}
